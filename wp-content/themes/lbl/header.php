c868ea69117bf8272f7c98f15ebe62848388c18b
Chip Lay <chiplay@mac.com>
2015-08-15 12:58:21 -0400

    More meta

--- wp-content/themes/lbl/header.php
+++ wp-content/themes/lbl/header.php
@@ -3,11 +3,20 @@
 	<head>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+    <title>Nursery Inspiration &amp; Baby Room Ideas | Lay Baby Lay</title>
+    <meta title="description" content="Inspiration to create spaces for your little ones that are full of imagination, color, and delight and maybe find a little encouragement in this crazy but wonderful journey of having babies and watching them grow."
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <meta property="og:title" content="Lay Baby Lay"/>
+    <meta property="og:type" content="website"/>
+    <meta property="og:description" content="Nursery Decor &amp; Baby Room Ideas"/>
+    <meta property="og:url" content="http://laybabylay.com"/>
+    <meta property="og:site_name" content="Lay Baby Lay"/>
+    <meta property="fb:app_id" content="179291298758035"/>
 
-    <link rel="stylesheet" href="/wp-content/themes/lbl/assets/styles/app.css?v=1.1.0">
-
-		<title>Nursery Inspiration &amp; Baby Room Ideas | Lay Baby Lay</title>
+    <link rel="stylesheet" href="/wp-content/themes/lbl/assets/styles/app.css?v=1.2.0">
 
 	</head>
 
