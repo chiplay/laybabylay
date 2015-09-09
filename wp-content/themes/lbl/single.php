60788d76972f4a100ec2f393156821dc1d7a125f
Chip Lay <chiplay@mac.com>
2015-09-09 15:12:24 -0400

    Update single to fix varnish/SEO issues

--- wp-content/themes/lbl/single.php
+++ wp-content/themes/lbl/single.php
@@ -4,75 +4,23 @@ Template Name: Single Post
 */
 ?>
 
-<?php
-
- preg_match('/alexa|bot|crawl(er|ing)|pinterest|facebookexternalhit|feedburner|google web preview|nagios|postrank|pingdom|slurp|spider|yahoo!|yandex|^$/i', $_SERVER['HTTP_USER_AGENT'], $matches);
-
-if (!$matches) {
-
-
-	get_template_part( 'index' );
-
-
-} else {
-
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
+<?php get_header(); ?>
 
 <?php while(have_posts()) : the_post(); ?>
 
-	<meta http-equiv="content-type" content="text/html; charset=utf-8">
-  <title><?php wp_title('&nbsp;&#124;&nbsp;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
-  <link rel="stylesheet" href="/wp-content/themes/lbl/assets/styles/app.css">
-  <meta property="og:title" content="<?php echo trim(wp_title('', false)); ?>"/>
-  <meta property="og:type" content="article"/>
-  <meta property="og:description" content="<?php echo get_the_excerpt(); ?>"/>
-  <?php
-  $postid = get_the_ID();
-  $images = get_children(array('post_parent' => $postid, 'post_type' => 'attachment', 'numberposts' => -1, 'orderby' => 'menu_order', 'post_mime_type' => 'image'));
-  foreach($images as $image) {
-    $attachmenthumb = wp_get_attachment_image_src($image->ID, 'full'); ?>
-    <meta property="og:image" content="<?php echo $attachmenthumb[0]; ?>"/>
-  <?php } ?>
-  <meta property="og:url" content="<?php the_permalink(); ?>"/>
-  <meta property="og:site_name" content="Lay Baby Lay"/>
-  <meta property="fb:app_id" content="179291298758035"/>
-  <meta property="article:author" content="https://www.facebook.com/joni.h.lay"/>
-  <meta property="article:publisher" content="https://www.facebook.com/laybabylay" />
-  <?php
-  $categories = get_the_category();
-  if($categories){
-  	foreach($categories as $category) { ?>
-  		<meta property="article:section" content="<?php echo $category->cat_name ?>"/>
-  	<?php }
-  } ?>
-  <?php
-  $posttags = get_the_tags();
-  if ($posttags) {
-    foreach($posttags as $tag) { ?>
-    <meta property="article:tag" content="<?php echo $tag->name ?>"/>
-    <?php }
-  }
-  wp_head(); ?>
-
-</head>
-
 <div id="main">
 
-	<article class="post">
+  <article class="post">
 
     <div class="container">
 
-			<header class="align-center">
+	<header class="align-center">
 
-				<h1><small><?php the_title(); ?></small></h1>
+	<h1><small><?php the_title(); ?></small></h1>
 
-				<span class="post_date"><?php the_time('l, F j, Y'); ?></span>
+	<span class="post_date"><?php the_time('l, F j, Y'); ?></span>
 
-			</header>
+	</header>
 
       <div class="content">
 
@@ -143,9 +91,4 @@ if (!$matches) {
 
 </div>
 
-  <?php wp_footer(); ?>
-
-</body>
-</html>
-
-<?php }   ?>
+<?php get_footer(); ?>
