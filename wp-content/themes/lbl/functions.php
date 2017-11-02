44111d5f651cf828c7a6d5f94b6db30ef3f85ece
Chip Lay <chiplay@mac.com>
2017-11-02 12:03:09 -0400

    Comment types?

--- wp-content/themes/lbl/functions.php
+++ wp-content/themes/lbl/functions.php
@@ -144,8 +144,6 @@ function acf_post_attributes(array $attributes, WP_Post $post) {
 	unset($attributes['record_index']);
 	unset($attributes['taxonomies_hierarchical']);
 	unset($attributes['post_author']);
-	unset($attributes['post_type']);
-	unset($attributes['post_type_label']);
 	unset($attributes['post_modified']);
 	unset($attributes['comment_count']);
 	unset($attributes['menu_order']);
