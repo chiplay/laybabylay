28630da07f92cfb44a1fbea54abd16dcf0ee001f
Chip Lay <chiplay@mac.com>
2017-11-20 22:48:23 -0500

    Clean up server.js

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 // server.js
-var express = require('express');
-var path = require('path');
-var compression = require('compression');
+const express = require('express');
+const path = require('path');
+const compression = require('compression');
 
-var app = express();
+const app = express();
 app.use(compression());
 
 // serve our static stuff like index.css
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // send all requests to index.html so browserHistory in React Router works
-app.get('*', function (req, res) {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-})
+});
 
-var PORT = process.env.PORT || 8080;
-app.listen(PORT, function() {
-  console.log('Production Express server running at localhost:' + PORT)
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Production Express server running at localhost: ${PORT}`);
 });
