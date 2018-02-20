51f5431d16c1567efeae22a621723b4139e39a0b
Chip Lay <chiplay@mac.com>
2018-02-20 12:54:28 -0500

    Remove Logrocket redux

--- app/scripts/store/configureStore.js
+++ app/scripts/store/configureStore.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import rootReducer from 'reducers';
-import LogRocket from 'logrocket';
+// import LogRocket from 'logrocket';
 
 const fslogger = store => next => action => {
   let result = next(action)
@@ -18,7 +18,7 @@ if (process.env.NODE_ENV === `development`) {
 
 if (process.env.NODE_ENV === `production`) {
   middlewares.push(fslogger);
-  middlewares.push(LogRocket.reduxMiddleware());
+//   middlewares.push(LogRocket.reduxMiddleware());
 }
 
 const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore);
