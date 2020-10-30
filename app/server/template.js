3ea4c19390493e3696f19c7540ceb13431a44c81
Chip Lay <chiplay@mac.com>
2020-10-30 16:40:03 -0400

    Add MS Clarity (test)

--- app/server/template.js
+++ app/server/template.js
@@ -53,6 +53,13 @@ function template(title, initialState = {}, content = "", styleTags = "", helmet
           g.clearUserCookie=function(){};
       })(window,document,window['_fs_namespace'],'script','user');
     </script>
+    <script type="text/javascript">
+        (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "3w739ur8z4");
+    </script>
   </head>
 
   <body>
